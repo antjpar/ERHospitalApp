99bf924fc9cd0000c263503f913f4a7e396583ec
Lidiia Lazebnikova <lidiia.lazebnikova@sumcumo.com>
2020-05-24 15:17:39 +0200

    add plus button

--- er-hospital/src/views/WaitingRoom.vue
+++ er-hospital/src/views/WaitingRoom.vue
@@ -1,5 +1,8 @@
 <template>
   <div :class="$style.table">
+    <v-btn icon outlined :class="$style.plusButton" large>
+      <v-icon color="white">add</v-icon>
+    </v-btn>
     <v-data-table :items="waiting" :headers="headers">
       <template v-slot:item.priorityType="{ item }">
         <v-icon :color="item.priorityType === 'P1' ? 'red' : 'teal'">
@@ -7,7 +10,7 @@
         </v-icon>
       </template>
       <template v-slot:item.arrived="{ item }">
-        <v-btn v-if="!item.arrived">ANNAHME</v-btn>
+        <v-btn v-if="!item.arrived">BESTÄTIGEN</v-btn>
       </template>
       <template v-slot:item.treated="{ item }">
         <v-btn v-if="!item.arrived">BEHANDELT</v-btn>
@@ -79,5 +82,11 @@ export default Vue.extend({
   .table {
     padding-top: 4rem;
     width: 80%;
+  }
+  .plusButton {
+    position: fixed;
+    bottom: 10vh;
+    right: 5vw;
+    background-color: #3f51b5;
   }
 </style>
